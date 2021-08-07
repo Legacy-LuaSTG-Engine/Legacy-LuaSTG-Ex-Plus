bbfe8b13dc9caa6f520c53d7850c390afc3e7f4e
璀境石 <Kuanlan@outlook.com>
2021-08-07 21:30:06 +0800

    fancy2d

--- cmake/TargetCommonOptions.cmake
+++ cmake/TargetCommonOptions.cmake
@@ -3,6 +3,11 @@ function(luastg_target_common_options __TARGET__)
         "/MP"
         "/utf-8"
     )
+    if(CMAKE_SIZEOF_VOID_P EQUAL 4)
+        target_compile_options(${__TARGET__} PRIVATE
+            "/arch:SSE2"
+        )
+    endif()
     set_target_properties(${__TARGET__} PROPERTIES
         C_STANDARD 17
         C_STANDARD_REQUIRED ON
