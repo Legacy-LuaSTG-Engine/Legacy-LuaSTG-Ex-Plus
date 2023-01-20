76cb06027f56fad09bcdd36a08509a7ce4458884
Kuanlan <Kuanlan@outlook.com>
2023-01-20 13:35:59 +0800

    jit模式？？？

--- luastg/AppFrame.cpp
+++ luastg/AppFrame.cpp
@@ -1364,7 +1364,7 @@ bool AppFrame::Init()LNOEXCEPT
 			LERROR("无法初始化Lua虚拟机");
 			return false;
 		}
-		if (0 != luaJIT_setmode(L, 0, LUAJIT_MODE_ENGINE | LUAJIT_MODE_ON))
+		if (0 == luaJIT_setmode(L, 0, LUAJIT_MODE_ENGINE | LUAJIT_MODE_ON))
 			LWARNING("无法启动JIT模式");
 		lua_gc(L, LUA_GCSTOP, 0);  // 初始化时关闭GC
 
